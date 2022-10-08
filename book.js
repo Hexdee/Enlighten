c1ef908fd117ac32d6c921bf74fcd43398287ae9
Hexdee <lukmansaheed79@gmail.com>
2022-10-08 15:48:51 +0100

    Quick fix

--- book.js
+++ book.js
@@ -1,7 +1,7 @@
 //Api tokens and private key
-const API_KEY = "8240e5368feb46af698cc4a178ec7859e91c23d1";
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEVDZTNiOTIxQzM4YjZmMUY5QzUzRThGNDMyMjc0MmM4MDk0MzU2NzQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDY2NzMyMDIwOCwibmFtZSI6IkVubGlnaHRlbiJ9.-_v_zFU7e-DU5vhu8thYtiCCvem9NULxNf43YSBYV18';
-const private_key = "2e5851f63e6787aeb26e664381c5c61ac49a7c26a800d08e4520eed418356506";
+const API_KEY = "";
+const token = '';
+const private_key = "";
 
 //Pin on ipfs using nft.storage
 import { NFTStorage, File } from "https://cdn.jsdelivr.net/npm/nft.storage/dist/bundle.esm.min.js";
